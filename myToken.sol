21030f01ec189c65c1fce676d04443d45916ea9a
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-02-18 17:32:07 +0530

    add constructor

--- myToken.sol
+++ myToken.sol
@@ -1,8 +1,13 @@
 contract myToken{
     uint256 totalSupply;
+    
+    string public name = "Dapp myToken";
+    string public symbol = "Dapp";
+    string public standard = "Dapp myToken v1";
+    
     mapping(address => uint256) public balanceOf;
     function myToken(uint _initialSupply) public{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-} 
+}
