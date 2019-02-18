f53147ef896ac47d154a5f679d6fd45b7c1e3a3f
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-02-18 18:50:15 +0530

    add transfer function

--- myToken.sol
+++ myToken.sol
@@ -2,7 +2,7 @@ contract myToken{
     uint256 totalSupply;
     
     string public name = "Dapp myToken";
-    string public symbol = "Dapp";
+    string public symbol = "DmT";
     string public standard = "Dapp myToken v1";
     
     mapping(address => uint256) public balanceOf;
@@ -10,4 +10,17 @@ contract myToken{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
+    
+    //function transfer
+        //exception if account doesn't have enough
+        //return boolean
+    //transfer event
+
+    function transfer (address _toAddress, uint256 _value) public returns(bool success){
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_toAddress] += _value;
+        
+        return true;
+    } 
 }
