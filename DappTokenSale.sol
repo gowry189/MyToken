ba4478630b91be4dc5a1081ea5bd31be665c93ba
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-03-09 18:25:18 +0530

    Add buyToken skelton

--- DappTokenSale.sol
+++ DappTokenSale.sol
@@ -3,9 +3,18 @@ import "myToken.sol";
 contract DappTokenSale{
     address admin;
     myToken public tokenContract;
-    function DappTokenSale(myToken _tokenContract)public{
+    uint256 tokenPrice;
+    function DappTokenSale(myToken _tokenContract,uint256 _tokenPrice)public{
         admin=msg.sender; //initialize admin
         tokenContract = _tokenContract; //assign tokenContract
-        //initialize tokenPrize
+        tokenPrice=_tokenPrice;//initialize tokenPrize
+    }
+    function buyTokens(uint _noOfTokens) public payable {
+        //require that the amount that we are buying for is equal to tokenprice
+        //require that there are enough tokens in the contract
+        //require that the transfer is succesaful
+        //keep track of no of tokens sold
+        //trigger sell event
+        
     }
 }
