9bc2632f67f070b830d2b13b4a15f98a699516bb
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-03-09 18:56:54 +0530

    Add function to multiply

--- DappTokenSale.sol
+++ DappTokenSale.sol
@@ -4,17 +4,26 @@ contract DappTokenSale{
     address admin;
     myToken public tokenContract;
     uint256 tokenPrice;
+    uint256 tokenSold;
+    event Sell(address _buyer, uint256 _amount);
     function DappTokenSale(myToken _tokenContract,uint256 _tokenPrice)public{
         admin=msg.sender; //initialize admin
         tokenContract = _tokenContract; //assign tokenContract
         tokenPrice=_tokenPrice;//initialize tokenPrize
     }
+    function mul(uint x, uint y) internal pure returns(uint z){
+        require(y==0 || (z=x*y)/y==x);
+    }
     function buyTokens(uint _noOfTokens) public payable {
         //require that the amount that we are buying for is equal to tokenprice
+        require(msg.value == mul(_noOfTokens,tokenPrice));
         //require that there are enough tokens in the contract
-        //require that the transfer is succesaful
-        //keep track of no of tokens sold
-        //trigger sell event
         
+        //require that the transfer is succesaful
+        
+        //keep track of no of tokens sold
+        tokenSold += _noOfTokens;
+        //trigger sell event
+        Sell(msg.sender,_noOfTokens);
     }
 }
